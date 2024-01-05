27fc15cf87597a0ea567006e756fb2d47bcc36ff
Luís Sitoe <luisvasco.jr@outlook.com>
2024-01-05 11:11:27 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ function addActiveLink() {
   const section = document.querySelectorAll("section[id]");
   section.forEach((section) => {
     const scrollY = window.scrollY,
-      sectionTop = section.offsetTop - 100,
+      sectionTop = section.offsetTop - 80,
       sectionHeight = section.offsetHeight,
       sectionId = section.getAttribute("id");
 
